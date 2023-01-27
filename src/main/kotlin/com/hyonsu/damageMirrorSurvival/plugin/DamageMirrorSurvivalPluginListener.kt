03e26b235ac1ed8eda74544cda937f6ea377bc59
HYONSU <admin@hyonsu.com>
2023-01-28 04:13:06 +0900

    모든 발사체 반영

--- src/main/kotlin/com/hyonsu/damageMirrorSurvival/plugin/DamageMirrorSurvivalPluginListener.kt
+++ src/main/kotlin/com/hyonsu/damageMirrorSurvival/plugin/DamageMirrorSurvivalPluginListener.kt
@@ -2,8 +2,8 @@ package com.hyonsu.damageMirrorSurvival.plugin
 
 import com.destroystokyo.paper.event.player.PlayerJumpEvent
 import org.bukkit.GameMode
-import org.bukkit.entity.Arrow
 import org.bukkit.entity.Player
+import org.bukkit.entity.Projectile
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
@@ -14,14 +14,15 @@ class DamageMirrorSurvivalPluginListener : Listener {
         var causePlayer: Player? = null
 
         if(e.damager is Player) causePlayer = e.damager as Player
-        if(e.damager is Arrow) {
-            val causeArrow = e.damager as Arrow
-            if(causeArrow.shooter is Player) causePlayer = causeArrow.shooter as Player
+        if(e.damager is Projectile) {
+            val causeProjectile = e.damager as Projectile
+            val shooter = causeProjectile.shooter
+            if(shooter == null) return
+
+            if(shooter is Player) causePlayer = causeProjectile.shooter as Player
         }
 
         if(causePlayer == null) return
-
-        causePlayer.sendMessage("${e.damage}")
 
         causePlayer.damage(e.damage)
     }
